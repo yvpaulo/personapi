d8deb0a2fd3f4acaa42ef3a49298124a642bbbf7
yvpaulo@yahoo.com.br <yvpaulo@yahoo.com.br>
2021-01-03 21:34:34 -0300

    commit initial

---
+++ src/main/java/com/restapi/personapi/controller/PersonController.java
@@ -0,0 +1,15 @@
+package com.restapi.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    public String getPerson(){
+        return "API WORK";
+    }
+}
---
+++ src/test/java/com/restapi/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
